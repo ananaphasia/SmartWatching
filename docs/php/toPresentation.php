717a9414ed0789cf47b418a1bff60d331decdcf3
as4mo3 <tosha7y@gmail.com>
2018-09-09 03:28:56 -0500

    DONE

---
+++ docs/php/toPresentation.php
@@ -0,0 +1,5 @@
+<?php
+if(isset($_POST['submitForm'])) {
+  header('Location: https://docs.google.com/presentation/d/1HcqCfA4I5UFAwmoKZy44wgP3H_Fxs7LkgGqAAx29oVU/edit#slide=id.g40e6767a65_1_17')
+}
+ ?>
